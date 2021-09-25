56a2e4ce8455aaa81f60146d3c724bd1487e9789
acm-sp85 <acontell@gmail.com>
2021-09-25 11:08:12 -0400

    Done.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -11,12 +11,28 @@ class PlantsController < ApplicationController
     plant = Plant.find_by(id: params[:id])
     render json: plant
   end
-
+  
   # POST /plants
   def create
     plant = Plant.create(plant_params)
+
     render json: plant, status: :created
   end
+  
+  #PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant 
+    
+  end
+  
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
 
   private
 
